c0281e7163ffe684c0840a0bd8c0c96624822e04
Alejandro Wang <awang21913@gmail.com>
2025-06-23 16:09:11 -0400

    somewhat working kafka server

--- backend/data-pipeline/producer.js
+++ backend/data-pipeline/producer.js
@@ -1,32 +1,48 @@
+require('dotenv').config();
 const KAFKA_BROKER = 'localhost:9092';
 const TOPIC = 'stock-prices';
 const { Kafka } = require('kafkajs');
-const { Polygon } = require('polygon.io');
+const { restClient } = require('@polygon.io/client-js');
 const axios = require('axios');
 
 async function fetchStockPrice(symbol) {
-    const apiKey = process.env.POLYGON_API_KEY;
-    const url = `https://api.polygon.io/v2/last/trade/${symbol}?apiKey=${apiKey}`;
+    const apiKey = "bYx0g7_YgYXQsq1msFy5zo8KmEAXpc1y";
+    console.log("API KEY: ", apiKey);
+    if (!apiKey) {
+        throw new Error('Polygon API key is not set in environment variables');
+    }
+    const rest = restClient(apiKey);
+
     try {
-        const response = await axios.get(url);
-        return response.data.last.price;
+        const response = await rest.reference.tickerDetails(
+            symbol,
+            {}
+        )
+        return response;
     } catch (error) {
         console.error(`Error fetching stock price for ${symbol}:`, error);
         throw error;
     }
 }
 async function run() {
-    const kafka = new Kafka({ clientId: 'producer', brokers: ['localhost:9092'] });
-    const producer = kafka.producer();
-
-    await producer.connect();
-    await producer.send({
-    topic: 'stock-ticker',
-    messages: [
-        { value: JSON.stringify({ symbol: 'AAPL', price: 187.45, timestamp: new Date().toISOString() }) },
-    ],
-    });
+    try {
+        const kafka = new Kafka({ clientId: 'producer', brokers: ['localhost:9092'] });
+        const producer = kafka.producer();
+        await producer.connect();
+        const response = await fetchStockPrice('AAPL'); // Example stock symbol
+        console.log('Fetched stock price:', response);
+        await producer.send({
+            topic: 'stock-ticker',
+            messages: [
+                {key: response.results.ticker, value: response.results.description}
+            ],
+        });
     await producer.disconnect();
+    } catch (error) {   
+        console.error('Error in run function:', error);
+        throw error;
+    }
+
 }
 
 run().catch(console.error);
